8ce2a6fdc2d528c1ff8f5b0b3b895a904cca1ac9
Hubert Kropidłowski <hubkro002@tutanota.com>
2024-03-25 02:36:32 +0100

    Player Basic Movement

--- Entities/PlayerCharacter/player_character.gd
+++ Entities/PlayerCharacter/player_character.gd
@@ -1,20 +1,26 @@
 extends CharacterBody2D
 
 @export var player_name = "n00b"
-@export var speed = 100
-@export var angular_speed := 5.0
-@export var friction = 1.0
-@export var health = 100
-@export var stamina = 100
-@export var hunger = 10.0
-@export var thrist = 10.0
+@export var speed = 250
+@export var friction = 0.2
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	var direction = Vector2(Input.get_action_strength("m_right") - Input.get_action_strength("m_left"), Input.get_action_strength("m_down") - Input.get_action_strength("m_up"))
-	if direction >= 1.0:
-		direction = direction.normalized()
-	var movement = direction * speed
+	#movement section ------------------------------------------------------
+	
+	var movement = Vector2()
+	movement.x = Input.get_action_strength("m_right") - Input.get_action_strength("m_left")
+	movement.y = Input.get_action_strength("m_down") - Input.get_action_strength("m_up")
+	
+	if movement.x >= 1 or movement.y >= 1 or movement.x >= -1 or movement.y >= -1:
+		movement = movement.normalized()
+	
+	var frictioned_vel = movement * speed
+	
+	velocity += (frictioned_vel - velocity) * friction #TODO add sprint option
 	move_and_slide()
+	print(movement)
+	
+	#movement section --------------------------------------------------------
