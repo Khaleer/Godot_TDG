7d3364fe1707d2cacdfc9e3fbd028846f921ceeb
Hubert Kropidłowski <hubkro002@tutanota.com>
2024-03-28 19:52:03 +0100

    interaction_fix

--- Interaction/InteractionManager.gd
+++ Interaction/InteractionManager.gd
@@ -4,9 +4,7 @@ extends Node2D
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var label = $Label
 
-
 const base_text = "[E] to "
-
 var active_areas = []
 var can_interact = true
 
@@ -21,7 +19,7 @@ func unregister_area(area: InteractionArea):
 func _process(delta):
 	
 	if active_areas.size() > 0 && can_interact:
-		active_areas.sort_custom(_sort_by_distance_to_player)
+		active_areas.sort_custom(func(x, y): return _sort_by_distance_to_player(x, y, player))
 		label.text = base_text + active_areas[0].action_name
 		label.global_position = active_areas[0].global_position
 		label.global_position.y -= 36
@@ -30,9 +28,10 @@ func _process(delta):
 		
 	else:
 		label.hide()
-		
-func _sort_by_distance_to_player(area1, area2):
-	var area1_to_player = player.global_posdition.distance_to(area1.global_position)
+
+
+func _sort_by_distance_to_player(area1, area2, player):
+	var area1_to_player = player.global_position.distance_to(area1.global_position)
 	var area2_to_player = player.global_position.distance_to(area2.global_position)
 	return area1_to_player < area2_to_player
 	
